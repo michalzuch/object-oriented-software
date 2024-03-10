e43abf3bb7b28e199a9a677e6af9b86ce2f24707
michał <51276321+michalzuch@users.noreply.github.com>
2024-03-10 23:48:33 +0100

    Paradigms - create procedure for sorting numbers
    - add procedure for sorting numbers
    - add procedure for printing output
    - save data generated by GenerateRandomNumbers procedure to a variable
    - refactor GenerateRandomNumbers to utilize new print procedure

--- Paradigms/Paradigms.pas
+++ Paradigms/Paradigms.pas
@@ -1,19 +1,60 @@
 
 Program Paradigms;
 
-Procedure GenerateRandomNumbers;
+Procedure PrintNumbers(Var numbers: Array Of integer);
 
 Var 
-  num: integer;
+  i: integer;
 Begin
-  Randomize;
-  For num := 1 To 50 Do
+  For i := 1 To 50 Do
     Begin
-      write(random(101), ' ');
+      write(numbers[i], ' ');
     End;
+  writeln;
 End;
 
+Procedure GenerateRandomNumbers(Var numbers: Array Of integer);
+
+Var 
+  i: integer;
+  num: integer;
 Begin
-  GenerateRandomNumbers;
+  write('Random numbers: ');
+  Randomize;
+  For i := 1 To 50 Do
+    Begin
+      num := random(101);
+      numbers[i] := num;
+    End;
+  PrintNumbers(numbers);
+End;
+
+Procedure SortNumbers(Var numbers: Array Of integer);
+
+Var 
+  i, j, temp: integer;
+Begin
+  write('Sorted numbers: ');
+  For i := 1 To 50 Do
+    Begin
+      For j := i + 1 To 50 Do
+        Begin
+          If numbers[i] > numbers[j] Then
+            Begin
+              temp := numbers[i];
+              numbers[i] := numbers[j];
+              numbers[j] := temp;
+            End;
+        End;
+    End;
+  PrintNumbers(numbers);
+End;
+
+Var 
+  numbers: array[1..50] Of integer;
+
+Begin
+  GenerateRandomNumbers(numbers);
+  SortNumbers(numbers);
   readln;
 End.
