215ca357d8b200a82c3630738c9bddd4028cf9a4
michał <51276321+michalzuch@users.noreply.github.com>
2024-03-11 00:13:22 +0100

    Paradigms - add parametered generator
    - add GenerateRandomNumbers with lower limit, upper limit and amount parameters
    - adjust other functions to dynamic size of the array

--- Paradigms/Paradigms.pas
+++ Paradigms/Paradigms.pas
@@ -1,12 +1,12 @@
 
 Program Paradigms;
 
-Procedure PrintNumbers(Var numbers: Array Of integer);
+Procedure PrintNumbers(Var numbers: Array Of integer; amount: integer);
 
 Var 
   i: integer;
 Begin
-  For i := 1 To 50 Do
+  For i := 1 To amount Do
     Begin
       write(numbers[i], ' ');
     End;
@@ -19,25 +19,47 @@ Var
   i: integer;
   num: integer;
 Begin
-  write('Random numbers: ');
+write('Random numbers: ');
   Randomize;
   For i := 1 To 50 Do
     Begin
       num := random(101);
       numbers[i] := num;
     End;
-  PrintNumbers(numbers);
+    PrintNumbers(numbers, 50);
 End;
 
-Procedure SortNumbers(Var numbers: Array Of integer);
+Procedure GenerateRandomNumbers(Var numbers: Array Of integer; lowerLimit: integer; upperLimit: integer; amount: integer);
+
+Var 
+  i: integer;
+  num: integer;
+Begin
+  If lowerLimit > upperLimit Then
+    Begin
+      writeln('Lower limit cannot be greater than upper limit');
+      exit;
+    End;
+
+  write('Random numbers: ');
+  Randomize;
+  For i := 1 To amount Do
+    Begin
+      num := random(upperLimit + 1 - lowerLimit) + lowerLimit;
+      numbers[i] := num;
+    End;
+  PrintNumbers(numbers, amount);
+End;
+
+Procedure SortNumbers(Var numbers: Array Of integer; amount: integer);
 
 Var 
   i, j, temp: integer;
 Begin
   write('Sorted numbers: ');
-  For i := 1 To 50 Do
+  For i := 1 To amount Do
     Begin
-      For j := i + 1 To 50 Do
+      For j := i + 1 To amount Do
         Begin
           If numbers[i] > numbers[j] Then
             Begin
@@ -47,14 +69,24 @@ Begin
             End;
         End;
     End;
-  PrintNumbers(numbers);
+  PrintNumbers(numbers, amount);
 End;
 
 Var 
-  numbers: array[1..50] Of integer;
+  numbers_1: array[1..50] Of integer;
+  numbers_2: array[1..5] Of integer;
+
 
 Begin
-  GenerateRandomNumbers(numbers);
-  SortNumbers(numbers);
+  writeln('Generating 50 random numbers between 0 and 100 and sorting them');
+  GenerateRandomNumbers(numbers_1);
+  SortNumbers(numbers_1, 50);
+  writeln;
+
+  writeln('Generating 5 random numbers between 25 and 35 and sorting them');
+  GenerateRandomNumbers(numbers_2, 25, 35, 5);
+  SortNumbers(numbers_2, 5);
+  writeln;
+
   readln;
 End.
